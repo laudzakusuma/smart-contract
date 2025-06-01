125216d9b9c642d473df5caabdf009584b056360
laudzakusuma <laudzaxie@gmail.com>
2025-06-01 19:06:44 +0700

    test

---
+++ test/HelloWorld.js
@@ -0,0 +1,21 @@
+const { expect } = require("chai");
+
+describe("HelloWorld", function () {
+  let helloWorld;
+
+  beforeEach(async function () {
+    const HelloWorld = await ethers.getContractFactory("HelloWorld");
+    helloWorld = await HelloWorld.deploy();
+    await helloWorld.deployed();
+  });
+
+  it("should return the default greeting", async function () {
+    expect(await helloWorld.getGreeting()).to.equal("Hello, Web3!");
+  });
+
+  it("should update the greeting", async function () {
+    const newGreeting = "Hello, Laudza!";
+    await helloWorld.setGreeting(newGreeting);
+    expect(await helloWorld.getGreeting()).to.equal(newGreeting);
+  });
+});
